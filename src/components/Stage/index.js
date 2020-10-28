3dd31fe592bba30872ba86e4b1508690881febd0
Cody Bennett <hi@codyb.co>
2020-10-28 06:35:00 -0500

    Updated lobby, fixed map texture depth.

--- src/components/Stage/index.js
+++ src/components/Stage/index.js
@@ -1,4 +1,4 @@
-import { Object3D, DoubleSide } from 'three';
+import { Object3D } from 'three';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import stages from './stages';
@@ -36,17 +36,7 @@ class Stage {
         node.position.y -= 1;
         this.navMesh = node;
       }
-
-      if (node.isMesh) {
-        node.material.side = DoubleSide;
-        node.frustumCulled = false;
-        node.material.transparent = true;
-      }
     });
-
-    if (name === 'lobby') {
-      model.scene.scale.set(0.5, 0.5, 0.5);
-    }
 
     return model.scene;
   }
